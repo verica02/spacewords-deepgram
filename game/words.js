784d41f41a4d67b1da757d7a869f8f242709549d
Verica Spaskovska <77109037+verica02@users.noreply.github.com>
2022-04-11 22:49:24 +0200

    Update words.js

--- game/words.js
+++ game/words.js
@@ -1,8 +1,8 @@
 const words = [
 	"planet", "astronaut", "star", "cosmos",  "universe", "spaceship", "moon", "galaxy",
-	 "orbit", "observatory", "landing", "satellite", "telescope", "gravity",
-	   "atmosphere", "binary", "dust", "light",  "space", "asteroid", "meteor",
-	
+	"orbit", "observatory", "landing", "satellite", "telescope", "gravity",
+	"atmosphere", "binary", "dust", "light",  "space", "asteroid", "meteor",
+	"solar","sun","eclipse","capsule", "sky"	
     ];
 
 const restart = [
